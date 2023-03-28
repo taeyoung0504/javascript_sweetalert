0d530fdb669b94d27687c2c65ba9ea338efa793f
taeyoung0504 <tang050405@gmail.com>
2023-03-28 17:15:19 +0900

    자바스크립트 예쁜 alert창 만들기(sweetalert2)

---
+++ sweetalert.js
@@ -0,0 +1,13 @@
+
+
+function alert() {
+
+    Swal.fire({
+        position: 'center', // alert창 위치 
+        icon: 'success',
+        title: '로그인 성공',
+        footer: '<a href="alert.html">뒤로가기</a>'        
+    })
+}
+
+ㄴ
